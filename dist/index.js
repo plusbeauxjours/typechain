3e1d242cf545fa919b43078db613a0c8e6379cb5
plusbeauxjours <plusbeauxjours@gmail.com>
2018-09-21 16:04:06 +0200

    #4 Types in Typescript

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+};
+console.log(sayHi("Minjae", 32, "male"));
+//# sourceMappingURL=index.js.map
