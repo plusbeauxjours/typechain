02f3dac08f49fc3e863cc9ab4aead612ce8aab0e
plusbeauxjours <plusbeauxjours@gmail.com>
2018-09-28 18:03:19 +0200

    #11 Validating Block Structure part Two

--- dist/index.js
+++ dist/index.js
@@ -29,6 +29,7 @@ const createNewBlock = (data) => {
     const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimeStamp);
     return newBlock;
 };
+const getHashforBlock = (aBlock) => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
 const isBlockValid = (candidateBlock, previousBlock) => {
     if (Block.validateStructure(candidateBlock)) {
         return false;
@@ -39,7 +40,16 @@ const isBlockValid = (candidateBlock, previousBlock) => {
     else if (previousBlock.hash !== candidateBlock.previousHash) {
         return false;
     }
-    else if ()
-        ;
+    else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+const addBlock = (candidateBlock) => {
+    if (isBlockValid(candidateBlock, getLatestBlock())) {
+        blockchain.push(candidateBlock);
+    }
 };
 //# sourceMappingURL=index.js.map
